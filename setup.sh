bb542751b41a929bcd29c5906c2a24b848433497
Shaharyar Ahmad <shaharyarahmad@users.noreply.github.com>
2018-05-27 18:42:59 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,7 @@ sudo usermod -aG docker vagrant
 #Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+wget https://raw.githubusercontent.com/shaharyarahmad/media-setup/master/docker-compose.yml
 
 #Docker Containers
 docker-compose up -d
