248f8953d6ff221408b0daebeefffd702926ba2a
Shaharyar Ahmad <shaharyarahmad@users.noreply.github.com>
2018-05-27 18:41:00 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,38 +4,24 @@ export LC_ALL=C.UTF-8
 #Update 
 apt-get update && apt-get upgrade
 
-#Apache
-apt-get install -y apache2
+#Docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo usermod -aG docker vagrant
 
-echo "ServerName localhost" | tee -a /etc/apache2/apache2.conf
+#Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-a2enmod rewrite
-service apache2 restart
-
-#PHP
-apt-add-repository ppa:ondrej/php
-apt-get update
-apt-get install -y php7.1
-apt-get install -y php-gd
-apt-get install -y php-curl
-apt-get install -y php-mcrypt
-apt-get install -y php-mbstring
-apt-get install -y php-xml
-apt-get install -y php-zip
-
-#MySQL
-apt-get install -y debconf-utils
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-apt-get install mysql-server php7.1-mysql mysql-common
-mysql -u root -proot -e "CREATE USER 'sandbox'@'%' IDENTIFIED BY 'sandbox';"
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'sandbox'@'%' WITH GRANT OPTION;"
-mysql -u root -proot -e "FLUSH PRIVILEGES;"
-service apache2 restart
-service mysql restart
-
-#Utilities
-apt-get install -y git
-apt-get install -y vim
-apt-get install -y zip
-apt-get install -y unzip
+#Docker Containers
+docker-compose up -d
