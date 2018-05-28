7e017f8e25faf7f1724aa354d347ec7f7e45d8cb
Shaharyar Ahmad <shaharyarahmad@users.noreply.github.com>
2018-05-28 22:19:20 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,39 +30,40 @@ sudo docker-compose up -d
 apt-get autoremove -y
 
 #Move Config for Jackett
-rm -f /config/jackett/Jackett/ServerConfig.json 
-rm -f /config/jackett/Jackett/Indexers/
-mv ~/config/jackett/Jackett/ServerConfig.json /config/jackett/Jackett/ServerConfig.json 
-mv ~/config/jackett/Jackett/Indexers/* /config/jackett/Jackett/Indexers/
+sudo rm -f /config/jackett/Jackett/ServerConfig.json 
+sudo rm -f /config/jackett/Jackett/Indexers/
+sudo mv /home/vagrant/config/jackett/Jackett/ServerConfig.json /config/jackett/Jackett/ServerConfig.json 
+sudo mkdir /config/jackett/Jackett/Indexers/
+sudo mv /home/vagrant/config/jackett/Jackett/Indexers/* /config/jackett/Jackett/Indexers/
 
 #Move Config for Radarr
-rm -f /config/radarr/config.xml 
-rm -f /config/radarr/nzbdrone.db
-rm -f /config/radarr/nzbdrone.db-shm
-rm -f /config/radarr/nzbdrone.db-wal
-rm -f /config/radarr/nzbdrone.pid
-mv ~/config/radarr/config.xml /config/radarr/config.xml 
-mv ~/config/radarr/nzbdrone.db /config/radarr/nzbdrone.db
-mv ~/config/radarr/nzbdrone.db-shm /config/radarr/nzbdrone.db-shm
-mv ~/config/radarr/nzbdrone.db-wal /config/radarr/nzbdrone.db-wal
-mv ~/config/radarr/nzbdrone.pid /config/radarr/nzbdrone.pid
+sudo rm -f /config/radarr/config.xml 
+sudo rm -f /config/radarr/nzbdrone.db
+sudo rm -f /config/radarr/nzbdrone.db-shm
+sudo rm -f /config/radarr/nzbdrone.db-wal
+sudo rm -f /config/radarr/nzbdrone.pid
+sudo mv /home/vagrant/config/radarr/config.xml /config/radarr/config.xml 
+sudo mv /home/vagrant/config/radarr/nzbdrone.db /config/radarr/nzbdrone.db
+sudo mv /home/vagrant/config/radarr/nzbdrone.db-shm /config/radarr/nzbdrone.db-shm
+sudo mv /home/vagrant/config/radarr/nzbdrone.db-wal /config/radarr/nzbdrone.db-wal
+sudo mv /home/vagrant/config/radarr/nzbdrone.pid /config/radarr/nzbdrone.pid
 
 #Move Config for Sonarr
-rm -f /config/sonarr/config.xml 
-rm -f /config/sonarr/nzbdrone.db
-rm -f /config/sonarr/nzbdrone.db-shm
-rm -f /config/sonarr/nzbdrone.db-wal
-rm -f /config/sonarr/nzbdrone.pid
-mv ~/config/sonarr/config.xml /config/sonarr/config.xml 
-mv ~/config/sonarr/nzbdrone.db /config/sonarr/nzbdrone.db
-mv ~/config/sonarr/nzbdrone.db-shm /config/sonarr/nzbdrone.db-shm
-mv ~/config/sonarr/nzbdrone.db-wal /config/sonarr/nzbdrone.db-wal
-mv ~/config/sonarr/nzbdrone.pid /config/sonarr/nzbdrone.pid
+sudo rm -f /config/sonarr/config.xml 
+sudo rm -f /config/sonarr/nzbdrone.db
+sudo rm -f /config/sonarr/nzbdrone.db-shm
+sudo rm -f /config/sonarr/nzbdrone.db-wal
+sudo rm -f /config/sonarr/nzbdrone.pid
+sudo mv /home/vagrant/config/sonarr/config.xml /config/sonarr/config.xml 
+sudo mv /home/vagrant/config/sonarr/nzbdrone.db /config/sonarr/nzbdrone.db
+sudo mv /home/vagrant/config/sonarr/nzbdrone.db-shm /config/sonarr/nzbdrone.db-shm
+sudo mv /home/vagrant/config/sonarr/nzbdrone.db-wal /config/sonarr/nzbdrone.db-wal
+sudo mv /home/vagrant/config/sonarr/nzbdrone.pid /config/sonarr/nzbdrone.pid
 
 #Move Config for Transmission
-rm -f /config/transmission/settings.json
-mv ~/config/transmission/settings.json /config/transmission/settings.json
+sudo rm -f /config/transmission/settings.json
+sudo mv /home/vagrant/config/transmission/settings.json /config/transmission/settings.json
 docker restart transmission
 
 #Clean Config Files
-rm -rf ~/config
+rm -rf /home/vagrant/config
